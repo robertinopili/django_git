0c0eae4752edbc736b4e9d5573a74a49413dda38
Robertino <roberpili02@gmail.com>
2023-04-24 20:44:14 -0300

    actualizado

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,20 @@ class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
+STATUS_CHOICES = (
+    ('R', 'Reviewed'),
+    ('N', 'Not reviewed'),
+    ('E', 'Error'),
+    ('A', 'Accepted')
+)
+
 class Website(models.Model):
     name = models.CharField(max_length=50)
     url = models.URLField()
     release_date = models.DateField()
     rating = models.IntegerField()
+    status = models.CharField(choices=STATUS_CHOICES, max_length=1)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Car(models.Model):
+    name = models.CharField(max_length=30, primary_key=True)
